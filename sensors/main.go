453b21d13cf295209d7db8d6168d4adcc369173a
Susanne Schmidt <sushee@outlook.com>
2019-02-03 21:48:37 +0100

    better random temperature float with one decimal

--- sensors/main.go
+++ sensors/main.go
@@ -2,30 +2,51 @@ package main
 
 import (
 	"fmt"
-
+  "time"
+  "os"
+  "encoding/json"
+  "math/rand"
 	"github.com/eclipse/paho.mqtt.golang"
 )
+
+type Payload struct {
+  SensorID string  `json:"sensorID"`
+  Type     string  `json:"type"`
+  Value    float64 `json:"value"`
+}
+
+func generatePayload() []byte {
+    rand_temp := float64(int((15.0 + rand.Float64() * 20.0) * 10.0)) / 10.0
+    payload   := Payload{Value:    rand_temp,
+                         Type:     "temperature",
+                         SensorID: "sensor-1"}
+
+    msg, _ := json.Marshal(payload)
+
+    return msg
+}
 
 func main() {
 	opts := mqtt.NewClientOptions()
   opts.AddBroker("0.0.0.0:1883")
   opts.SetClientID("sensor nr one")
-
 	client := mqtt.NewClient(opts)
 
   token := client.Connect();
 
   if token.Wait() && token.Error() != nil {
-
-		panic(token.Error())
+      fmt.Println(token.Error())
+      os.Exit(1)
 	}
 
-	for i := 0; i < 5; i++ {
-		msg   := fmt.Sprintf("19", i)
-		token := client.Publish("readings/temperature", 0, false, msg)
-		token.Wait()
+	for {
+      msg := generatePayload()
+      token := client.Publish("readings/temperature", 0, false, msg)
+      token.Wait()
+      time.Sleep(5 * time.Second)
 	}
 
+  client.Disconnect(250)
 }
 
 
